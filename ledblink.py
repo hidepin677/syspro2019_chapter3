50fc6df1caf9194f4479ef1905b20259b01860c9
hidepin677 <3jtanizaki@gmail.com>
2019-10-29 12:45:51 +0900

    追加

---
+++ ledblink.py
@@ -0,0 +1,18 @@
+import time
+import RPi.GPIO as GPIO
+
+ 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+GPIO.output(14, GPIO.HIGH)
+time.sleep(10)
+
+while (True) :
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(0.5)
+
+GPIO.cleanup()
